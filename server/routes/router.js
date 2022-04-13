83385c5139491f6d338d544a7cb6152d47134211
HarshDubey <harshchandradubey@gmail.com>
2022-04-13 17:40:16 +0530

    Error solved due to not found controller page

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,39 +1,38 @@
 const express = require("express");
 const route = express.Router();
 const service = require("../services/render");
-const controller = require("../controller/controller");
-
+// const controller = require("../controller/controller");
 
 /**
  * @description Root Page
  * @method GET/
  */
-route.get('/',service.homeRoute);
+route.get("/", service.homeRoute);
 
 /**
  * @description About Page
  * @method GET/
  */
-route.get('/about',service.aboutRoute);
+route.get("/about", service.aboutRoute);
 
 /**
  * @description Services Page
  * @method GET/
  */
-route.get('/services',service.servicesRoute);
+route.get("/services", service.servicesRoute);
 
 /**
  * @description Portfolio Page
  * @method GET/
  */
-route.get('/portfolio',service.portfolioRoute);
+route.get("/portfolio", service.portfolioRoute);
 
 /**
  * @description Contact Page
  * @method GET/
  */
-route.get('/contact',service.contactRoute);
+route.get("/contact", service.contactRoute);
 
 // API
-route.post("/sendmail", controller.sendEmail);
+// route.post("/sendmail", controller.sendEmail);
 module.exports = route;
